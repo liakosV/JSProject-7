4d229136c2aaeff70ef61dc72f4f2360a321ab58
LiakosV <billiak@hotmail.com>
2025-02-14 20:20:44 +0200

    feat: modules and sets added

---
+++ modules/src/lib2.js
@@ -0,0 +1,11 @@
+// named exports
+const PI = 3.14
+const EPSILON = 0.000005
+const API_URL = "https://codingfactory.aueb.gr"
+
+export {PI, EPSILON, API_URL}
+
+// default export - only one default export
+const SEC_PER_MIN = 60
+export default SEC_PER_MIN
+
