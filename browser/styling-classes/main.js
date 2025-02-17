272ee4c3bbabf773f3e72209cce16f1887960e8d
LiakosV <billiak@hotmail.com>
2025-02-17 23:41:23 +0200

    feat: browser files created

---
+++ browser/styling-classes/main.js
@@ -0,0 +1,9 @@
+const container = document.getElementById('container')
+const btn = document.getElementById('btn')
+
+btn.addEventListener('click' , function() {
+  container.classList.toggle('container')
+  container.classList.toggle('text-center')
+  container.innerHTML = '<p>Hello JS with CSS classes</p>'
+})
+
