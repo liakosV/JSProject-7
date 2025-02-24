eaa67e4d5199f19eea0e8579285690daf92f8368
LiakosV <billiak@hotmail.com>
2025-02-24 12:06:23 +0200

    feat notes-app js added

--- browser/notes-app/main.js
+++ browser/notes-app/main.js
@@ -1,4 +1,4 @@
-const daysGr = ['Κυριακη', "Δευτερα", "Τριτη", "Τεταρτη", "Πεμπτη", "Παρασκευη", "Σαββατο"]
+const daysGR = ['Κυριακη', "Δευτερα", "Τριτη", "Τεταρτη", "Πεμπτη", "Παρασκευη", "Σαββατο"]
 
 const monthsGR = ['Ιανουαρίου', 'Φεβρουαρίου', 'Μαρτίου', 'Απριλίου', 'Μαΐου', 'Ιουνίου', 'Ιουλίου', 'Αυγούστου', 'Σεπτεμβρίου', 'Οκτωβρίου', 'Νοεμβρίου', 'Δεκεμβρίου']
 
@@ -40,7 +40,7 @@ function printGRDate() {
 }
 
 function onInsertHandler(data) {
-  if(data.note) return
+  if (!data.note) return
   insertNote(data)
   reset()
 }
@@ -52,14 +52,35 @@ function insertNote(note) {
 }
 
 function renderNotes() {
-  const item = document.querySelector('#noteWrapper')
+  const container = document.getElementById('notesWrapper')
 
-  item.innerHTML = notes.map((notes) => `<div id="${'noteTemplate' + note.key}" class="flex justify-between items-center px-[10px] py-[2px] border-b border-black">
+  container.innerHTML = notes.map((note) =>
+     `<div id="${'noteTemplate' + note.key}" class="flex justify-between items-center px-[10px] py-[2px] border-b border-black">
+
   <div id="${'noteInfo' + note.key}" class="flex items-center">
+
     <input type="checkbox" id="${'noteCheck' + note.key}" onclick="strikeThrough(${note.key})" class="w-[25px] h-[25px] mr-[5px]" ${note.softDeleted ? 'checked' : ''}>
-    <label id="${'noteTxt' + note.key}" for="${'noteCheck' + note.key}" class="w-[200px] max-h-[150px] leading-[1.2rem] overflow-hidden break-words whitespace-normal text-base ${note.softDeleted ? 'line-through text-gray-500': ''}">${note.note}</label>
+
+    <label id="${'noteTxt' + note.key}" for="${'noteCheck' + note.key}" class="w-[200px] max-h-[150px] leading-[1.2rem] overflow-hidden break-words whitespace-normal text-base ${note.softDeleted ? 'line-through text-gray-500': ''}">
+    ${note.note}
+    </label>
   </div>
-  <button type="button" id="${'noteDelBtn' + note.key}" class="w-[35px] h-[35px]" onclick="deleteNote(${note.key})">X</button>
-</div>`).join("")
-  
+  <button type="button" id="${'noteDelBtn' + note.key}" class="w-[35px] h-[35px]" onclick="deleteNote(${note.key})">
+    X
+  </button>
+</div>`).join("") 
+}
+
+function strikeThrough(key) {
+  notes = notes.map(note => note.key === key ? {...note, softDeleted: !note.softDeleted} : {...note})
+  renderNotes()
+}
+
+function deleteNote(key) {
+  notes = notes.filter(note => note.key !== key)
+  renderNotes()
+}
+
+function reset() {
+  document.querySelector('#inputNote').value = ""
 }
